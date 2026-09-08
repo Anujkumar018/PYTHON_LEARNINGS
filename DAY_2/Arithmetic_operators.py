233ad270b9513c3bb2cc1cfc6d42165256cb84ec
Anujkumar018 <Kumaranujofc@gmail.com>
2026-09-08 21:15:30 +0530

    Add Arithmetic operators implementation with addition, subtraction, multiplication, division, remainder, exponentiation, and floor division

---
+++ DAY_2/Arithmetic_operators.py
@@ -0,0 +1,68 @@
+
+#addition
+a=int(input("Enter first number for addition:"))                       #taking two numbers as input from user
+b=int(input("enter second number:"))
+add=a+b                                                                 #adding the two numbers
+print("The sum of input a and b =",add)
+
+
+
+#Subtraction
+a=int(input("Enter first number for subtract:"))                       #taking two numbers as input from user
+b=int(input("enter second number:"))
+sub=a-b                                                                 #subtracting the two numbers using - operator
+print("The sum of input a and b =" ,sub)
+
+
+
+#multiplication
+a=int(input("Enter first number for multiplication:"))
+b=int(input("Enter second number:"))
+multiplication=a*b                                                      #using * operater for multiplication
+print("multiplication of number a amnd b is =",multiplication)
+
+
+
+#Division 
+a=int(input("Enter divident number:"))                                   #taking two numbers as input from user
+b=int(input("enter divisor number:"))
+Division=a/b                                                              #using / operator for division
+print("The division of input a and b =" ,Division)
+
+
+
+#remainder
+a=int(input("Enter dividend number:"))                                   #taking diividend and divisor number as input
+b=int(input("Enter divisor number:"))   
+remainder=a%b                                                            #using modulus operator to find remainder
+print("The remainder of a divided by b is =",remainder)
+
+
+
+#Exponent 
+a=int(input("Enter first number to find exponet:"))                       #taking diividend and divisor number as input
+b=int(input("Enter second number:"))   
+Exponent=a**b                                                             #using power operator to find exponent
+print("The remainder of a divided by b is=",Exponent)
+
+
+
+
+#Floor devision
+a=int(input("Enter dividend number for FD:"))                             #taking diividend and divisor number as input
+b=int(input("Enter divisor number:"))   
+remainder=a//b                                                            #using floor division operator 
+print("The remainder of a divided by b is =", remainder)
+
+
+
+
+
+
+
+
+
+
+
+
+
