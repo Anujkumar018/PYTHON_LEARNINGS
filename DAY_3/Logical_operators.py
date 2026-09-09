b78a7e3b8a1c625459fb6b17bb707a29cc8c43a3
Anujkumar018 <Kumaranujofc@gmail.com>
2026-09-09 17:20:10 +0530

    Add truth table examples for logical operators (AND, OR, NOT)

---
+++ DAY_3/Logical_operators.py
@@ -0,0 +1,25 @@
+# Truth table for AND
+
+print(True and True)    
+print(True and False)   
+print(False and True)   
+print(False and False)  
+
+print()
+
+
+# Truth table for OR
+
+print(True or True)    
+print(True or False)   
+print(False or True)    
+print(False or False)  
+
+
+print()
+
+
+# Truth table for NOT
+
+print(not True)         
+print(not False)       
