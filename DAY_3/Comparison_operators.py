c65b418f9f5aa1cf28d32b653955aedca17b92ae
Anujkumar018 <Kumaranujofc@gmail.com>
2026-09-09 17:15:07 +0530

    Add comparison operators examples demonstrating Boolean returns

---
+++ DAY_3/Comparison_operators.py
@@ -0,0 +1,36 @@
+#Comparison operators return only Boolean values ie True or False.
+
+# Equal to (==)
+age = 18
+required_age = 18
+print(age == required_age)   
+
+
+# Not equal to (!=)
+password = "hello"
+correct_password = "python"
+print(password != correct_password)   
+
+
+# Greater than (>)
+marks = 39
+pass_marks = 40
+print(marks > pass_marks)   
+
+
+# Less than (<)
+temperature = 20
+maximum_temperature = 30
+print(temperature < maximum_temperature)   
+
+
+# Greater than or equal to (>=)
+balance = 1000
+minimum_balance = 1000
+print(balance >= minimum_balance)   
+
+
+# Less than or equal to (<=)
+items = 5
+maximum_items = 4
+print(items <= maximum_items)   
