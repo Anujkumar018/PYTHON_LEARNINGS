a1ebc2c995af779c26ced058073965bce144413a
Anujkumar018 <Kumaranujofc@gmail.com>
2026-09-09 16:59:11 +0530

    Add variable and assignment operators examples

---
+++ DAY_3/Assignment_operators.py
@@ -0,0 +1,22 @@
+#all types of Assingment operators
+
+x = 10     # Assign
+
+x += 5     # Add and assign
+x -= 5     # Subtract and assign
+x *= 5     # Multiply and assign
+x /= 5     # Divide and assign
+x //= 5    # Floor-divide and assign
+x %= 5     # Remainder and assign
+x **= 5    # Power and assign
+
+
+# Bitwise assignment operators
+
+y=11
+
+y &= 5     # Bitwise AND and assign
+y |= 5     # Bitwise OR and assign
+y ^= 5     # Bitwise XOR and assign
+y >>= 1    # Right-shift and assign
+y <<= 1    # Left-shift and assign
