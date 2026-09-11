4ab1a231aaf19f94d8f99425b3eb3406a4f04ebc
Anujkumar018 <Kumaranujofc@gmail.com>
2026-09-11 23:27:47 +0530

    Add examples for string functions including len, endswith, count, capitalize, find, and replace

---
+++ DAY_5/string_fun().py
@@ -0,0 +1,36 @@
+#len() function use to check the length of an string
+s="hello"
+print(len(s))       #5
+
+#endswith() function use to check if the last value of the string end with the text or not
+
+t="python"
+print(t.endswith("z"))      #False
+
+#count() function use to check total count of occurence of a charecter in the string 
+
+r="wool"
+count=r.count("o")
+print(count)            #2
+
+#capitalize() capitalize the first charecter of an string
+a="code"
+capitalize=a.capitalize()
+print(capitalize)         #Code
+
+
+#find() function is used find the index of first occurence of the charecter first time 
+b="bee"
+ind=b.find("ee")
+print(ind)                  #1
+
+
+#replace() function replace the old word with a new word in the string
+
+c="hope"
+rep=c.replace('h','r')
+print(rep)          #rope
+
+
+
+
