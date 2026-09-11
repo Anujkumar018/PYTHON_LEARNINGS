881b150d8d02b3f50478ee936f3b079d048d32db
Anujkumar018 <Kumaranujofc@gmail.com>
2026-09-11 23:16:26 +0530

    Add string slicing examples demonstrating basic and advanced techniques

---
+++ DAY_5/Strings_slicing.py
@@ -0,0 +1,30 @@
+#can be primarily write a string in these ways -:
+
+a="hello python x-"
+b='hello python y-'
+c='''hello python z'''
+
+print(a,b,c)
+
+#slicing an string 
+
+#with the help of slicing feature we can have the part of string 
+y="ABCDEFGH"                    #length of string=8 starting with index 0-7
+z=y[0:5]                        #Start at index 0 and stop before index 5   
+print(z)                        #ABCDE
+
+#slicing with skip value
+
+ex="this is an example"
+p=ex[0:9:1]              #format for slicing with a skip value [start:stop:steps]
+print(p)                 #this is a
+
+
+
+
+
+
+
+
+
+
