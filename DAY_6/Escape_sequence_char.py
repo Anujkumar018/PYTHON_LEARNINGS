5179af0d75845082044dede40e1ce6fb959065d9
Anujkumar018 <Kumaranujofc@gmail.com>
2026-09-13 23:24:58 +0530

    Add examples demonstrating escape sequence characters in Python

---
+++ DAY_6/Escape_sequence_char.py
@@ -0,0 +1,7 @@
+#sequence of characters after backslash "\" are called escape sequence characters.
+
+print("Hello\nWorld")      # \n  New line       | Output: Hello (newline) World
+print("Hello\tWorld")      # \t  Tab space       | Output: Hello   World
+print("She said \"Hi\"")   # \"  Double quote    | Output: She said "Hi"
+print('It\'s fine')        # \'  Single quote    | Output: It's fine
+print("C:\\Users\\Guest")  # \\  Backslash       | Output: C:\Users\Guest   
