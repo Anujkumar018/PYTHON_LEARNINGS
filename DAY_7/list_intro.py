8e7bc1e6f6f74fb249a4b5f5a413110d0419f296
Anujkumar018 <Kumaranujofc@gmail.com>
2026-09-22 23:03:04 +0530

    Add examples demonstrating list creation, indexing, and element modification in Python

---
+++ DAY_7/list_intro.py
@@ -0,0 +1,36 @@
+#A list is an ordered, mutable collection of different datatype written inside square brackets '[]'
+elements=["book",False,100,"rakesh"]
+print(elements)         #['book', False, 100, 'rakesh']
+
+
+
+"""List Indexing--
+Indexing means accessing an element using its position
+indexing starts from 0,"""
+
+numbers = [10, 20, 30, 40, 50]
+
+print(numbers[0])           #10
+print(numbers[2])           #30
+print(numbers[4])           #50
+
+#negative indexing
+print(numbers[-4])          #20
+
+
+
+#changing list element
+numbers = [10, 20, 30, 40]
+
+numbers[1] = 200
+
+print(numbers)              #[10, 200, 30, 40]
+
+
+
+
+
+
+
+
+
